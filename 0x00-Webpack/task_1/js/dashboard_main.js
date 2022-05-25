2b67aad85277c3152181b83963de8fecc0fdfc9b
Sebatian <sebastian@nufi.mx>
2022-05-25 17:58:04 -0500

    1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,22 @@
+import $ from 'jQuery';
+import _ from 'lodash';
+
+let counter = 0;
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+const updateCounter = () => {
+	counter++;
+	// $('#count').text(`${counter} clicks on the button`);
+	document.getElementById(
+		'count',
+	).innerHTML = `${counter} clicks on the button`;
+};
+
+document
+	.querySelector('button')
+	.addEventListener('click', _.debounce(updateCounter, 1000));
