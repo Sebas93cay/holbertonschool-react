fb288fdb35b12681b7075a8074e8d75be0b370cd
Sebatian <sebastian@nufi.mx>
2022-06-11 19:15:51 -0500

    Fix 2

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,9 +1,8 @@
-export default function accessImmutableObject(object, array) {
-  let result = object;
-  for (let key of array) {
-    result = result[key];
-    if (result === undefined) return undefined;
-  }
-  if (typeof result === 'object') return new Map(Object.entries(result));
-  return JSON.stringify(result);
-}
+import { fromJS } from 'immutable';
+
+const accessImmutableObject = (object, array) => {
+  const obj = fromJS(object);
+  return obj.getIn(array, undefined);
+};
+
+export default accessImmutableObject;
